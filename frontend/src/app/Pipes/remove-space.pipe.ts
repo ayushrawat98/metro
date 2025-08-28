627a048d678d02a3fe399f8ec87c4af13b6ddf64
ayushrawat98 <aayushrawat0@gmail.com>
2025-08-28 10:56:51 +0530

    Pipe update

--- frontend/src/app/Pipes/remove-space.pipe.ts
+++ frontend/src/app/Pipes/remove-space.pipe.ts
@@ -6,7 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RemoveSpacePipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): string {
-    return value.replace(/\n+/g, "\n")
+    return value.replace(/(\r\n|\n|\r)+/g, "\r\n")
   }
 
 }
+
+
