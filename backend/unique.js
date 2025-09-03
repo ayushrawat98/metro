44b658f4a2b025ec548574ef4eaf950073c3e96a
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-04 01:18:39 +0530

    UI update

--- backend/unique.js
+++ backend/unique.js
@@ -4,7 +4,6 @@ const ip = require('request-ip')
 exports.uniqueName = async (req, res, next) => {
 	const shortName = uniqueNamesGenerator({
 		dictionaries: [adjectives, animals], // colors can be omitted here as not used
-		style: 'capital',
 		seed: ip.getClientIp(req)
 	});
 	req.uniqueName = shortName
