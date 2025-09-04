83e026c1fe5239627455e8d838d78de93b72f43f
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-04 13:43:16 +0530

    UI update

--- frontend/src/app/Services/internaldata.service.ts
+++ frontend/src/app/Services/internaldata.service.ts
@@ -11,7 +11,7 @@ export class InternaldataService {
 
   currentBoard = signal<string>('')
   currentThread = signal<number|string>(-1)
-  currentTheme = signal<"light"|"dark">("light")
+  currentTheme = signal<"light"|"dark">("dark")
 
 //   showGlobalProgressBar = signal<boolean>(false)
   globalProgressBarValue = signal<number>(0)
