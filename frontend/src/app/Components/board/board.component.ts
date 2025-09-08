4e7160ad8ad84bad0399788f823e1ef2667dcf58
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-08 23:01:22 +0530

    UI update

--- frontend/src/app/Components/board/board.component.ts
+++ frontend/src/app/Components/board/board.component.ts
@@ -17,8 +17,8 @@ export class BoardComponent {
 		{name : 'fa', desc : '/fashion/'},
 		{name : 'g', desc : '/technology/'},
 		{name : 'dharm', desc : '/धर्म/'},
-		{name : 'out', desc : '/बाहर जाओ और	घास	स्पर्श करो/'},
-		{name : 'media', desc : '/music & movies/'},
+		{name : 'out', desc : '/बाहर जाओ/'},
+		{name : 'media', desc : '/music & movie/'},
 		{name : 'meta', desc : '/abuse admin/'},
 	]
 
