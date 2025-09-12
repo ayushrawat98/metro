577886f17a2080a84b1509c1f653238c255dd8f5
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-12 07:15:41 +0530

    filter update

--- frontend/src/app/Pipes/filter-word.pipe.ts
+++ frontend/src/app/Pipes/filter-word.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterWordPipe implements PipeTransform {
 
   transform(value: string, ...args: unknown[]): string {
-    return value.replace(/p+a+j+e+t+/gi, "p****t");
+    return value.replace(/p+a+j+e+t+/gi, "p****t").replace(/r+a+p+e+/gi, "r**e");
   }
 
 }
