afb1a847f4cb8090bf9e34da025d4bd2f19c6910
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-16 23:16:12 +0530

    nsfw update

--- frontend/src/app/Services/nsfw.service.ts
+++ frontend/src/app/Services/nsfw.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as nsfwjs from 'nsfwjs';
+import * as tf from "@tensorflow/tfjs";
+tf.enableProdMode();
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class NsfwService {
   async loadModel() {
     if (!this.model) {
       // loads the default model from nsfwjs CDN
-      this.model = await nsfwjs.load("MobileNetV2");
+      this.model = await nsfwjs.load("/");
     }
   }
 
