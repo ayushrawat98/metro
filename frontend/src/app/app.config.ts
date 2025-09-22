285923975c13945f63df4290d619980f79339191
ayushrawat98 <aayushrawat0@gmail.com>
2025-09-23 05:24:37 +0530

    zone.js added

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,5 +5,5 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZonelessChangeDetection(), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
 };
